3d95d2387fa4ef50ed2417ab0a4a7c00d264c634
sheeDev <sheedev@gmail.com>
2014-07-27 20:03:35 +0200

    1st commit on gitHub !

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import time, thread
+
+def myfunction(string, sleeptime, lock, *args):
+    #write down here the 1st function 
+    pass
+
+if __name__ == "__main__":
+# write down here the main file 
+    pass
+
+# eGit setup 
+# http://wiki.eclipse.org/EGit/User_Guide#GitHub_Tutorial
+# http://benjsicam.me/blog/how-to-setup-eclipse-git-plugin-egit-for-github-part-2-tutorial/
